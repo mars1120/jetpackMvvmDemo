c9b304a631f23cd135a92afc40f5f642708b58b3
mars1120 <peach414@gmail.com>
2019-09-04 15:43:40 +0800

    getAllWords改為回傳LiveData

--- app/src/main/java/com/ithome11/jetpackmvvmdemo/util/WordDao.kt
+++ app/src/main/java/com/ithome11/jetpackmvvmdemo/util/WordDao.kt
@@ -1,5 +1,6 @@
 package com.ithome11.jetpackmvvmdemo.util
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -10,9 +11,12 @@ import androidx.room.Query
 interface WordDao {
     // Query 填寫sql語法
 
-    // ORDER BY  Column_name  排序結果  ASC 代表由小往大  DESC 大到小
+    /**
+     *  1.ORDER BY  Column_name  排序結果  ASC 代表由小往大  DESC 大到小
+     *  2.getAllWords =返回包有List<Word> 的 LiveData
+     **/
     @Query("SELECT * FROM word_table ORDER BY word ASC")
-    fun getAllWords():List<WordEntity>
+    fun getAllWords(): LiveData<List<WordEntity>>
 
     //Insert有保留語法
     @Insert(onConflict = OnConflictStrategy.IGNORE)
