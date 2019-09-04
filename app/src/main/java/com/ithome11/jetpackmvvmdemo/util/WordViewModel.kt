a7dacf998e57317780300aa55177bcf48c96be9b
mars1120 <peach414@gmail.com>
2019-09-04 16:57:13 +0800

    新增viewModel

---
+++ app/src/main/java/com/ithome11/jetpackmvvmdemo/util/WordViewModel.kt
@@ -0,0 +1,23 @@
+package com.ithome11.jetpackmvvmdemo.util
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class WordViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository: WordRepository
+    val allWords: LiveData<List<WordEntity>>
+
+    init {
+        //從WordRoomDatabase引用唯一的wordDao構築WordRepository
+        val wordDao = WordRoomDatabase.getDatabase(application).WordDao()
+        repository = WordRepository(wordDao)
+        allWords = repository.allWords
+    }
+    fun insertWord(word:WordEntity)= viewModelScope.launch {
+        repository.insert(word)
+    }
+
+}
