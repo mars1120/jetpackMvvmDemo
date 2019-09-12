694a310736ae731851ee4cb6e19b9c3dc5844cca
mars1120 <peach414@gmail.com>
2019-09-12 17:41:27 +0800

    新建一個增加word用的頁面

--- app/src/main/java/com/ithome11/jetpackmvvmdemo/main/MainActivity.kt
+++ app/src/main/java/com/ithome11/jetpackmvvmdemo/main/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.ithome11.jetpackmvvmdemo.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ithome11.jetpackmvvmdemo.R
 import com.ithome11.jetpackmvvmdemo.util.WordListAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,5 +18,14 @@ class MainActivity : AppCompatActivity() {
         val wordAdapter = WordListAdapter(this)
         recyclerView.adapter = wordAdapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, NewWordActivity::class.java)
+            startActivityForResult(intent, newWordActivityRequestCode)
+        }
+    }
+
+    companion object {
+        const val newWordActivityRequestCode = 1
     }
 }
