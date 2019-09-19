18155621b87a7e0a5a7bbefc785a9e36bfd3413a
mars1120 <peach414@gmail.com>
2019-09-19 22:17:32 +0800

    串接LiveData

--- app/src/main/java/com/ithome11/jetpackmvvmdemo/main/ChronoActivity.kt
+++ app/src/main/java/com/ithome11/jetpackmvvmdemo/main/ChronoActivity.kt
@@ -1,18 +1,30 @@
 package com.ithome11.jetpackmvvmdemo.main
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.ithome11.jetpackmvvmdemo.R
-import kotlinx.android.synthetic.main.activity_chrono.*
 
 class ChronoActivity : AppCompatActivity() {
+    private lateinit var chronometerViewModel: ChronometerViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chrono)
-        val chronometerViewModel = ViewModelProviders.of(this).get(ChronometerViewModel::class.java)
-        chronometer.base = chronometerViewModel.startTime
-        chronometer.start()
+        chronometerViewModel = ViewModelProviders.of(this).get(ChronometerViewModel::class.java)
+        subscribe()
+    }
+
+    private fun subscribe() {
+        val elapsedTimeObserver = Observer<Long> { aLong ->
+            val newText = aLong.toString() + " seconds elapsed"
+            (findViewById<View>(R.id.text_time) as TextView).setText(newText)
+            Log.d("ChronoActivity", "Updating timer")
+        }
+        chronometerViewModel.elapsedTime.observe(this, elapsedTimeObserver)
     }
 }
--- app/src/main/java/com/ithome11/jetpackmvvmdemo/main/ChronometerViewModel.kt
+++ app/src/main/java/com/ithome11/jetpackmvvmdemo/main/ChronometerViewModel.kt
@@ -1,8 +1,33 @@
 package com.ithome11.jetpackmvvmdemo.main
 
 import android.os.SystemClock
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.util.*
 
 class ChronometerViewModel : ViewModel() {
-    var startTime: Long = SystemClock.elapsedRealtime()
+    companion object {
+        private val ONE_SECOND = 1000
+    }
+
+    private val mElapsedTime = MutableLiveData<Long>()
+    private val mInitialTime: Long
+    val elapsedTime: LiveData<Long>
+        get() = mElapsedTime
+
+    init {
+        mInitialTime = SystemClock.elapsedRealtime()
+        val timer = Timer()
+
+        // 更新經過的時間
+        timer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                val newValue = (SystemClock.elapsedRealtime() - mInitialTime) / 1000
+                //通知資料更新
+                mElapsedTime.postValue(newValue)
+            }
+        }, ONE_SECOND.toLong(), ONE_SECOND.toLong())
+
+    }
 }
