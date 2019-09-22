dc3651976cc66669c2cef2a0f1f29e2a2ef0b6a1
mars1120 <peach414@gmail.com>
2019-09-22 20:33:04 +0800

    1.初始化stage01Activity

---
+++ app/src/main/java/com/ithome11/jetpackmvvmdemo/main/s01/Stage01Activity.kt
@@ -0,0 +1,13 @@
+package com.ithome11.jetpackmvvmdemo.main.s01
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ithome11.jetpackmvvmdemo.R
+
+class Stage01Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stage01)
+    }
+}
