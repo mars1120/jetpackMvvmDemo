ac411af5d1e357df729c2a40dcfefc0688173acc
mars1120 <peach414@gmail.com>
2019-09-22 21:26:07 +0800

    更新viewmodel

--- app/src/main/java/com/ithome11/jetpackmvvmdemo/main/s01/Stage01Activity.kt
+++ app/src/main/java/com/ithome11/jetpackmvvmdemo/main/s01/Stage01Activity.kt
@@ -2,12 +2,28 @@ package com.ithome11.jetpackmvvmdemo.main.s01
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.ithome11.jetpackmvvmdemo.R
+import com.ithome11.jetpackmvvmdemo.databinding.ActivityStage01Binding
 
 class Stage01Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stage01)
+
+        val viewModel = ViewModelProviders.of(this).get(Stage01ViewModel::class.java)
+
+        DataBindingUtil.setContentView<ActivityStage01Binding>(this, R.layout.activity_stage01)
+            .let {
+                it.setLifecycleOwner(this)
+                it.viewModel = viewModel
+            }
+
+        val setInvisibleResult = Observer<String?> { viewModel.visibleResult.value = false }
+        viewModel.left.observe(this, setInvisibleResult)
+        viewModel.right.observe(this, setInvisibleResult)
     }
 }
---
+++ app/src/main/java/com/ithome11/jetpackmvvmdemo/main/s01/Stage01ViewModel.kt
@@ -0,0 +1,27 @@
+package com.ithome11.jetpackmvvmdemo.main.s01
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class Stage01ViewModel : ViewModel() {
+    val left = MutableLiveData<String>("")
+    val right = MutableLiveData<String>("")
+    val result = MutableLiveData<String>("")
+    val visibleResult = MutableLiveData<Boolean>().apply { value = false }
+
+    fun plus() {
+        val leftNum =
+            left.value.toString().toIntOrNull() ?: run { visibleResult.value = false; return }
+        val rightNum =
+            right.value.toString().toIntOrNull() ?: run { visibleResult.value = false; return }
+        result.value = (leftNum + rightNum).toString()
+        visibleResult.value = true
+    }
+
+    fun clear() {
+        left.value = ""
+        right.value = ""
+        result.value = ""
+        visibleResult.value = false
+    }
+}
