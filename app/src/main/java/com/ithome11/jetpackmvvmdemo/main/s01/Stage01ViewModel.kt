5d67ed3691ba520cdc96007fe7b428f640914a6b
mars1120 <peach414@gmail.com>
2019-09-23 23:06:07 +0800

    1.result 改為MediatorLiveData 觀察left跟right的數據

--- app/src/main/java/com/ithome11/jetpackmvvmdemo/main/s01/Stage01ViewModel.kt
+++ app/src/main/java/com/ithome11/jetpackmvvmdemo/main/s01/Stage01ViewModel.kt
@@ -1,12 +1,32 @@
 package com.ithome11.jetpackmvvmdemo.main.s01
 
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class Stage01ViewModel : ViewModel() {
     val left = MutableLiveData<String>("")
     val right = MutableLiveData<String>("")
-    val result = MutableLiveData<String>("")
+    val result = MediatorLiveData<String>().apply {
+        value = ""
+        val plus: (String?) -> Unit = plus@{
+            val leftNum = left.value?.toIntOrNull()
+            if (leftNum == null) {
+                visibleResult.value = false
+                return@plus
+            }
+            val rightNum = right.value?.toIntOrNull()
+            if (rightNum == null) {
+                visibleResult.value = false
+                return@plus
+            }
+            value = (leftNum + rightNum).toString()
+            visibleResult.value = true
+        }
+        addSource(left, plus)
+        addSource(right, plus)
+    }
+
     val visibleResult = MutableLiveData<Boolean>().apply { value = false }
 
     fun plus() {
