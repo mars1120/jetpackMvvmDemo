6ff83c0279cf90257219aadefa18b1039cd06c65
mars1120 <peach414@gmail.com>
2019-10-03 21:43:36 +0800

    初始化專案

---
+++ app/src/main/java/com/ithome11/jetpackmvvmdemo/main/s04/S04Activity.kt
@@ -0,0 +1,13 @@
+package com.ithome11.jetpackmvvmdemo.main.s04
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ithome11.jetpackmvvmdemo.R
+
+class S04Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_s04)
+    }
+}
